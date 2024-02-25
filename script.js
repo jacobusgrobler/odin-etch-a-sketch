06f49f454f79ccf2181023d1419bca836d125206
Jodan <jacobusgrobler01@gmail.com>
2024-02-25 15:33:09 +0200

    Partial page layout and functionality

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 'use strict'
 
+const buttons = {
+    size: document.querySelector('.size'),
+    color: document.querySelector('.color'),
+    random: document.querySelector('.random'),
+    reset: document.querySelector('.reset'),
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    gridBlockSize()
-    console.log('nono')
+    gridBlockSize();
+
+    //let userInputSize = document.querySelector('.size')
+    buttons.size.addEventListener('click', function () {
+    let sizeSelected = userSize();
+    console.log(sizeSelected)
+    gridBlockSize(sizeSelected);
+    })
 })
 
 function gridBlockSize(size = 16) {
@@ -17,4 +31,20 @@ function gridBlockSize(size = 16) {
         board.insertAdjacentElement('beforeend', div);
     }
 
+}
+
+function userSize() {
+    let input = prompt("Please chose a size between 16 and 100");
+    let userresponce = document.querySelector('.userresponce');
+
+    if (input == ''){
+        userresponce.textContent = "Default Selected";
+    }
+    else if (input < 16 || input > 100) {
+        userresponce.textContent = "Select between 16 and 100"
+    }
+    else{
+        userresponce.textContent = "Play On Cinnabon"
+        return input;
+    }
 }
